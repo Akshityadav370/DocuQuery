17b6a8502d7a43aa7f1a2887f1a87b670ea573f8
Akshit Yadav <akshit07032001@gmail.com>
2024-01-29 21:29:40 +0530

    setup AWS S3

--- docu-query/src/components/FileUpload.tsx
+++ docu-query/src/components/FileUpload.tsx
@@ -1,15 +1,27 @@
 "use client";
+import { uploadToS3 } from "@/lib/s3";
 import { Inbox } from "lucide-react";
 import React from "react";
 import { useDropzone } from "react-dropzone";
 
 const FileUpload = () => {
   const { getRootProps, getInputProps } = useDropzone({
-    accept: {"application/pdf": [".pdf"]},
+    accept: { "application/pdf": [".pdf"] },
     maxFiles: 1,
-    onDrop: (acceptedFile) => {
-        console.log(acceptedFile);
-    }
+    onDrop: async (acceptedFile) => {
+      console.log(acceptedFile);
+      const file = acceptedFile[0];
+      if (file.size > 10 * 1024 * 1024) {
+        alert("PDFs upto 10MB are only allowed!");
+        return;
+      }
+      try {
+        const data = await uploadToS3(file);
+        console.log('data:', data);
+      } catch (error) {
+        console.log(error);
+      }
+    },
   });
   return (
     <div className="p-2 bg-white rounded-xl">
@@ -19,10 +31,10 @@ const FileUpload = () => {
             "border-dashed border-2 rounded-xl cursor-pointer bg-gray-50 py-8 flex justify-center items-center flex-col",
         })}
       >
-        <input {...getInputProps()}/>
+        <input {...getInputProps()} />
         <>
-        <Inbox className="w-10 h-10 text-blue-500"/>
-        <p className="mt-2 text-sm text-slate-400">Drop PDF Here</p>
+          <Inbox className="w-10 h-10 text-blue-500" />
+          <p className="mt-2 text-sm text-slate-400">Drop PDF Here</p>
         </>
       </div>
     </div>
