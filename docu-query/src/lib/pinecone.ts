b3f4f3f0b186c007559116ea4872162e49baaff2
Akshit Yadav <akshit07032001@gmail.com>
2024-01-30 22:06:49 +0530

    split & segment the pdf

--- docu-query/src/lib/pinecone.ts
+++ docu-query/src/lib/pinecone.ts
@@ -1,6 +1,10 @@
 import { Pinecone } from "@pinecone-database/pinecone";
 import { downloadFromS3 } from "./s3-server";
 import { PDFLoader } from "langchain/document_loaders/fs/pdf";
+import {
+  Document,
+  RecursiveCharacterTextSplitter,
+} from "@pinecone-database/doc-splitter";
 
 let pinecone: Pinecone | null = null;
 
@@ -13,16 +17,48 @@ export const getPineconeClient = async () => {
   return pinecone;
 };
 
+type PDFPage = {
+  pageContent: string;
+  metadata: {
+    loc: { pageNumber: number };
+  };
+};
+
 export async function loadS3IntoPinecone(fileKey: string) {
   // 1. Obtain pdf -> download from cloud & read pdf
   console.log("downloading s3 into file system");
   const file_name = await downloadFromS3(fileKey);
   if (!file_name) {
-    throw new Error('could not download from s3');
+    throw new Error("could not download from s3");
   }
   const loader = new PDFLoader(file_name);
-  const pages = await loader.load();
-  console.log(pages);
-  return pages;
+  const pages = (await loader.load()) as PDFPage[];
 
+  // 2. Split & Segment the pdf into smaller documents
+  // pages = Array(13)
+  const documents = await Promise.all(pages.map(prepareDocument));
+  // documents = Array(1000)
+}
+
+export const truncateStringByBytes = (str: string, bytes: number) => {
+  const enc = new TextEncoder();
+  return new TextDecoder("utf-8").decode(enc.encode(str).slice(0, bytes));
+};
+
+async function prepareDocument(page: PDFPage) {
+  let { pageContent, metadata } = page;
+  // replace new line with empty string
+  pageContent = pageContent.replace(/\n/g, "");
+  // Split the docs/page
+  const splitter = new RecursiveCharacterTextSplitter();
+  const docs = await splitter.splitDocuments([
+    new Document({
+      pageContent,
+      metadata: {
+        pageNumber: metadata.loc.pageNumber,
+        text: truncateStringByBytes(pageContent, 36000),
+      },
+    }),
+  ]);
+  return docs;
 }
